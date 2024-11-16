35cdb11b22419cf2b92977c32a1e07498d9a17ba
Shivam <shivamjessingani033@gmail.com>
2024-11-16 19:36:42 +0500

    Update question10.c

--- Assignment 2/question10.c
+++ Assignment 2/question10.c
@@ -5,30 +5,30 @@ int main(){
     printf("Enter length of the pattern(Odd Number): ");
     scanf("%d", &n);
 
-    int spc1 = (n*2)-2;
-    int spc2 = 0;
+    int spc1 = (n*2)-2;//initializing outer space in start of patter for exmaple if n=5 so the space in starting are 8
+    int spc2 = 0;//intializing inner space
 
 
-    for(i = 0; i<(n-1); i++){
-        for(j=0; j<spc1; j++){
+    for(i = 0; i<(n-1); i++){//dividing the pattern in 4 parts
+        for(j=0; j<spc1; j++){// loop for spaces
             printf(" ");
         }
-        printf("*");
+        printf("*");//one star in first line
         for(j=0; j<spc2; j++){
             printf(" ");
         }
-        if(j!=0) printf("*");
+        if(j!=0) printf("*");//a star after inner spaces
         printf("\n");
         
-        spc1--;
-        if(i == 0) spc2++;
-        else spc2+=2;
-    }
+        spc1--;//decreasing outer spaces by 1
+        if(i == 0) spc2++;//in second line there is only one inner space
+        else spc2+=2;//inner space is increasing by 2
+    }//end for loop
 
-    for(i=0; i<n; i++){
+    for(i=0; i<n; i++){//stars in nth line
         printf("*");
     }
-    for(i=0; i<spc2; i++){
+    for(i=0; i<spc2; i++){middle spaces in nth line
         printf(" ");
     }
     for(i=0; i<n; i++){
@@ -36,10 +36,10 @@ int main(){
     }
     printf("\n");
     
-    spc2 = (spc2*2)-1;
-    spc1 = 1;
+    spc2 = (spc2*2)-1;//increment in inner spaces
+    spc1 = 1;//outer space is 1
 
-    for(i=0; i<n-1; i++){
+    for(i=0; i<n-1; i++){//3rd part of pattern
         for(j=0; j<spc1; j++){
             printf(" ");
         }
@@ -49,14 +49,14 @@ int main(){
         }
         printf("*");
         printf("\n");
-        spc2-=2;
-        spc1++;
+        spc2-=2;//decrement in inner space by 2
+        spc1++;//increment in outer space by 1
 
     }
     
-    spc1 = n-2;
-    spc2 = 1;
-    for(i=0; i<n-2; i++){
+    spc1 = n-2;//outer space
+    spc2 = 1;//inner space
+    for(i=0; i<n-2; i++){//last part pattern
         for(j=0; j<spc1; j++){
             printf(" ");
         }
@@ -74,15 +74,15 @@ int main(){
         }
         printf("*");
         printf("\n");
-        spc1 --;
-        spc2+=4;
+        spc1 --;//decrement in outer space by 1
+        spc2+=4;//increment in inner space by 4
     }
 
-    spc2+=2;
-    printf("*");
-    for(j=0; j<spc2; j++){
+    spc2+=2;//inner space = previos inner space +2
+    printf("*");//the last line
+    for(j=0; j<spc2; j++){//loop for spaces
         printf(" ");
     }
     printf("*");
     return 0;
-}
+}//end main
